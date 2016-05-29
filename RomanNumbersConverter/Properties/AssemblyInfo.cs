0882a625d6c95d7cdb54c448f4a323ae09a612ed
Arthur <arthur.peka@outlook.com>
2016-05-29 12:06:32 +0300

    Providing program's description

--- RomanNumbersConverter/Properties/AssemblyInfo.cs
+++ RomanNumbersConverter/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RomanNumbersConverter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Converting from Roman numbers to Indo-Arabic and vice versa")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RomanNumbersConverter")]
